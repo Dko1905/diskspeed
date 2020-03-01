bbb42c9ac8ff837d8a329c9f5524fb41c2b0bbde
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-03-01 17:30:40 +0100

    Added read test. And made the main.c.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h> // For strtoull (string to unsinged long long) & exit()
+#include <string.h> // for seperating things
 
 #include "write.h"
 #include "read.h"
@@ -8,13 +9,47 @@
 void print_usage();
 
 int main(int argc, char *argv[]){
-	/*
 	int option;
-	while((option = getopt(argc, argv, "o:m:hwrdv")) != -1){
+
+	char* outputfile;
+	int write_flag = 1;
+	int read_flag = 1;
+	int delete_flag = 0;
+	int verbose = 0;
+	unsigned long long chunk_size = 1000;
+	unsigned long long chunks = 1;
+
+	while((option = getopt(argc, argv, "o:b:hdv")) != -1){
 		switch(option){
 			case 'h': // help
 				print_usage();
 				exit(0);
+				break;
+			case 'o':
+				outputfile = optarg;
+				if(!outputfile){
+					perror("output file is invalid.");
+					exit(1);
+				}
+				break;
+			case 'd':
+				delete_flag = 1;
+				break;
+			case 'r':
+				write_flag = 0;
+				break;
+			case 'w':
+				read_flag = 0;
+				break;
+			case 'v':
+				verbose = 1;
+				break;
+			case 'b':
+				sscanf(optarg, "%llu:%llu", &chunk_size, &chunks);
+				if(chunks < 1 || chunk_size < 1){
+					fprintf(stderr, "b argument is invalid\n");
+					exit(1);
+				}
 				break;
 			default:
 				printf("Wrong option\n");
@@ -23,9 +58,33 @@ int main(int argc, char *argv[]){
 				break;
 		}
 	}
-	*/
-	write_test("test.bin", 1000, 10, 1, 0);
-	read_test("test.bin", 1000, 10, 1, 0);
+	double read_result = 0, write_result = 0;
+	if(read_flag && !write_flag)
+		write_result = write_test("test.bin", chunk_size*chunks, 1, verbose, delete_flag);
+	else if(write_flag && !read_flag)
+		read_result = read_test("test.bin", chunk_size, chunks, verbose, delete_flag);
+	else if(write_flag && read_flag){
+		write_result = write_test("test.bin", chunk_size, chunks, verbose, 0);
+		read_result = read_test("test.bin", chunk_size, chunks, verbose, delete_flag);
+	}
+	double total_data_written = (double)chunks * (double)chunk_size;
+	printf("chunk_size:%llu\nchunks:%llu\ntotal data written:%llu\n", chunk_size, chunks, chunk_size*chunks);
+	if(write_result != 0)
+		printf("\033[94mWrite test:\033[0m\nTotal time: %fs\nTB/s: %f\nGB/s: %f\nMB/s: %f\nKB/s: %f\n", 
+			write_result,
+			(total_data_written/write_result)/1000000000000.0,
+			(total_data_written/write_result)/1000000000.0,
+			(total_data_written/write_result)/1000000.0,
+			(total_data_written/write_result)/1000.0
+		);
+	if(read_result != 0)
+				printf("\033[94mRead test:\033[0m\nTotal time: %fs\nTB/s: %f\nGB/s: %f\nMB/s: %f\nKB/s: %f\n", 
+			read_result,
+			(total_data_written/read_result)/1000000000000.0,
+			(total_data_written/read_result)/1000000000.0,
+			(total_data_written/read_result)/1000000.0,
+			(total_data_written/read_result)/1000.0
+		);
 }
 
 void print_usage(){
--- src/read.c
+++ src/read.c
@@ -6,6 +6,8 @@
 #include <unistd.h> // for close()
 #include <sys/stat.h> // for fstat() (size of file)
 
+#include <time.h> // for clock_gettime & timespec struct
+
 #include "read.h"
 
 double read_test(
@@ -15,7 +17,7 @@ double read_test(
 	int verbose,
 	int dflag // delete file after test
 ){
-	int file_handler = open(filename, O_SYNC | O_WRONLY);
+	int file_handler = open(filename, O_SYNC | O_RDONLY);
 
 	if(file_handler < 0){
 		perror("open() failed");
@@ -33,7 +35,53 @@ double read_test(
 	else if(verbose){
 		printf("fstat() finished\n");
 	}
-	printf("Size is %ld\n", st.st_size);
+	if(st.st_size < chunk_size*chunks){
+		fprintf(stderr, "input file is too small\n");
+		exit(2);
+	}
 
-	return 0.0;
+	char* chunk = (char*)malloc(chunk_size);
+	if(chunk == NULL){
+		perror("malloc() failed");
+		exit(1);
+	}
+
+	struct timespec start;
+	struct timespec end;
+	int start_result, end_result;
+
+	start_result = clock_gettime(CLOCK_REALTIME, &start);
+
+	for(int n = 1; n <= chunks; n++){
+		if(read(file_handler, chunk, chunk_size) == -1){
+			perror("read() failed");
+			exit(1);
+		}
+	}
+	
+	end_result = clock_gettime(CLOCK_REALTIME, &end);
+	if(end_result == -1 || start_result == -1){
+		perror("clock_gettime() failed");
+		exit(1);
+	}
+	else if(verbose){
+		printf("read() finished\n");
+	}
+	double start_double = 1.0*start.tv_sec+1e-9*start.tv_nsec;
+	double end_double = 1.0*end.tv_sec+1e-9*end.tv_nsec;
+	double dif_double = end_double-start_double;
+
+	close(file_handler);
+	free(chunk);
+	if(dflag){
+		if(remove(filename) == -1){
+			perror("Failed to remove file");
+			exit(1);
+		}
+		else if(verbose){
+			printf("removed file\n");
+		}
+	}
+
+	return dif_double;
 }
