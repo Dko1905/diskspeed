63082278b9920e1cbaa4484a72436c78186e4543
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-23 20:19:46 +0200

    Added open

--- src/opt.c
+++ src/opt.c
@@ -1,9 +1,16 @@
 #include <unistd.h> // For something //TODO check this
-#include <stdio.h> // IO/
+#include <stdio.h> // printf
 #include <stdlib.h> // Standard things
 #include <getopt.h> // For getopt and if gnu also getopt_long
 #include <errno.h> // For errno
 #include <string.h> // For strncpy
+/**
+ * This is commented out because we need to include them manually in the if statements
+ * 
+ * #include <fcntl.h> // For open() 
+ * #include <sys/stat.h> // For file permissions
+ * #include <sys/types.h>
+ */
 
 #include "write.h"
 #include "read.h"
@@ -30,6 +37,13 @@ void print_usage();
 
 
 #ifdef __gnu_linux__
+#define __USE_GNU //TODO check this
+
+#include <fcntl.h> // For open() 
+#include <sys/stat.h> // For file permissions
+#include <sys/types.h>
+
+
 // Check for getopt_long support
 
 #define STRMAX 255
@@ -40,7 +54,8 @@ static struct option long_options[] = {
 	{"read", no_argument, NULL, 'r'},
 	{"delete", no_argument, NULL, 'd'},
 	{"output", required_argument, NULL, 'o'},
-	{"byte", required_argument, NULL, 'b'}
+	{"byte", required_argument, NULL, 'b'},
+	{"help", no_argument, NULL, 'h'}
 };
 
 int parse_and_run(int argc, char* argv[]){
@@ -53,6 +68,7 @@ int parse_and_run(int argc, char* argv[]){
 	int read_flag = 0;
 	int delete_flag = 0;
 	char* output_filename = malloc(STRMAX);
+	output_filename[STRMAX] = '\0'; // For security reasons
 	size_t chunk_size = -1, chunk_amount = -1;
 	ERR_CHECK(output_filename == NULL, "Failed to alocate buffers for output_filename\n");
 
@@ -80,6 +96,10 @@ int parse_and_run(int argc, char* argv[]){
 				int sr = sscanf(optarg, "%llu:%llu", &chunk_size, &chunk_amount);
 				ERR_CHECK(sr < 2, "sscanf failed\n");
 				break;
+			case 'h':
+				print_usage();
+				return 0;
+				break;// This is not needed
 			default:
 				//print_usage();
 				break;
@@ -90,6 +110,26 @@ int parse_and_run(int argc, char* argv[]){
 	TODO Setup opening file and read/write tests
 	*/
 
+	// File open section
+	int fd = creat(output_filename, O_EXCL | S_IRUSR | S_IWUSR);
+	if(fd < 0){
+		if(errno == EEXIST){
+			LOG("Created file successfully\n");
+		}
+		else{
+			ERR_CHECK(1==1, "Failed to create file");
+		}
+	}
+	close(fd);
+	fd = open(output_filename, O_TRUNC | O_SYNC | O_DIRECT);
+	ERR_CHECK(fd < 0, "Failed to open %s", output_filename);
+
+
+	LOG("Opened %s\n", output_filename);
+
+	int cr = close(fd);
+	ERR_CHECK(cr < 0, "Failed to close %s", output_filename);
+
 	free(output_filename);
 	return 0;
 }
@@ -98,6 +138,10 @@ int parse_and_run(int argc, char* argv[]){
 #elif __unix__
 // Use getopt for unix
 
+int parse_and_run(int argc, char* argv[]){
+	ERR("Not implemented\n");
+	return 1;
+}
 
 #else
 // Unsupported platform, say error message
