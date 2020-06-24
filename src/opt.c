57065656c3497a9a3f9d75b6f719c3a95b0b084f
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-24 11:05:09 +0200

    Created UNIX implementation

--- src/opt.c
+++ src/opt.c
@@ -36,7 +36,7 @@
 void print_usage();
 
 
-#ifdef __gnu_linux__
+#ifdef __gnu_linux2__
 #define __USE_GNU
 
 #include <fcntl.h> // For open() 
@@ -186,9 +186,132 @@ int parse_and_run(int argc, char* argv[]){
 #elif __unix__
 // Use getopt for unix
 
+#include <fcntl.h> // For open() 
+#include <sys/stat.h> // For file permissions
+#include <sys/types.h>
+
+#define STRMAX 255
+
 int parse_and_run(int argc, char* argv[]){
-	ERR("Not implemented\n");
-	return 1;
+	int option;
+
+	// Flags
+	int verbose_flag = 0;
+	int write_flag = 0;
+	int read_flag = 0;
+	int delete_flag = 0;
+	size_t chunk_size = -1, chunk_amount = -1;
+	int random_flag = 1;
+
+	char* output_filename = malloc(STRMAX);
+	output_filename[STRMAX-1] = '\0'; // For security reasons
+	ERR_CHECK(output_filename == NULL, "Failed to alocate buffers for output_filename\n");
+
+	while(
+		(option = getopt(argc, argv, "vwrdo:b:n")) != -1
+	){
+		switch(option){
+			case 'v':
+				verbose_flag = 1;
+				break;
+			case 'w':
+				write_flag = 1;
+				break;
+			case 'r':
+				read_flag = 1;
+				break;
+			case 'd':
+				delete_flag = 1;
+				break;
+			case 'o':
+				strncpy(output_filename, optarg, STRMAX-1);
+				output_filename[STRMAX-1] = '\0'; // For security reasons
+				break;
+			case 'b': ; // Fix bad c standard
+				int sr = sscanf(optarg, "%llu:%llu", &chunk_size, &chunk_amount);
+				ERR_CHECK(sr < 2, "sscanf failed\n");
+				break;
+			case 'h':
+				print_usage();
+				return 0;
+				break;// This is not needed
+			case 'n':
+				random_flag = 0;
+				break;
+			default:
+				//print_usage();
+				break;
+		}
+	}
+
+	if(chunk_amount < 0 || chunk_size < 0){
+		ERR("Invalid usage\n");
+		print_usage();
+		return 1;
+	}
+	if(!read_flag && !write_flag){
+		read_flag = 1;
+		write_flag = 1;
+	}
+
+	// File open section
+	int fd2 = creat(output_filename, O_EXCL | S_IRUSR | S_IWUSR);
+	if(fd2 < 0){
+		if(errno == EEXIST){
+			LOG("Created file successfully\n");
+		}
+		else{
+			ERR_CHECK(1==1, "Failed to create file");
+		}
+	}
+	int cr = close(fd2);
+	if(cr < 0){
+		perror("Failed to close file");
+	}
+	int fd = open(output_filename, O_TRUNC | O_SYNC | O_RDWR);
+	ERR_CHECK(fd < 0, "Failed to open %s", output_filename);
+
+	LOG("Opened %s\n", output_filename);
+
+	double write_result = -1, read_result = -1;
+
+	if(write_flag){
+		int wtr = write_test(fd, chunk_size, chunk_amount, verbose_flag, random_flag, &write_result);
+		ERR_CHECK(wtr != 0, "Failed to do write_test");
+		LOG("Write test done\n");
+	}
+	if(read_flag){
+		lseek(fd, 0, SEEK_SET); // Seek to start to read
+		int rtr = read_test(fd, chunk_size, chunk_amount, verbose_flag, &read_result);
+		ERR_CHECK(rtr != 0, "Failed to do read_test");
+		LOG("Read test done\n");
+	}
+	cr = close(fd);
+	LOG("Closed %s\n", output_filename);
+	ERR_CHECK(cr < 0, "Failed to close %s", output_filename);
+
+	double total_data_written = (double)chunk_size * (double)chunk_amount;
+	if(write_flag){
+		printf("\033[94mWrite test:\033[0m\nTotal time: %fs\nTB/s: %f\nGB/s: %f\nMB/s: %f\nKB/s: %f\n", 
+			write_result,
+			(total_data_written/write_result)/1000000000000.0,
+			(total_data_written/write_result)/1000000000.0,
+			(total_data_written/write_result)/1000000.0,
+			(total_data_written/write_result)/1000.0
+		);
+	}
+	if(read_flag){
+		printf("\033[94mRead test:\033[0m\nTotal time: %fs\nTB/s: %f\nGB/s: %f\nMB/s: %f\nKB/s: %f\n", 
+			read_result,
+			(total_data_written/read_result)/1000000000000.0,
+			(total_data_written/read_result)/1000000000.0,
+			(total_data_written/read_result)/1000000.0,
+			(total_data_written/read_result)/1000.0
+		);
+	}
+
+	free(output_filename);
+	return 0;
 }
 
 #else
